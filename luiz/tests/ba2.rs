01b14a4d723089be400aba002ea01e174176c96e
Luiz Irber <luiz.irber@gmail.com>
2019-04-06 10:52:53 -0700

    avoid \n in tests

--- luiz/tests/ba2.rs
+++ luiz/tests/ba2.rs
@@ -8,7 +8,14 @@ use tempfile::NamedTempFile;
 #[test]
 fn ba2a_test() -> Result<(), Box<std::error::Error>> {
     let mut file = NamedTempFile::new()?;
-    writeln!(file, "3 1\nATTTGGC\nTGCCTTA\nCGGTATC\nGAAAATT")?;
+    writeln!(
+        file,
+        "3 1
+ATTTGGC
+TGCCTTA
+CGGTATC
+GAAAATT"
+    )?;
 
     let mut cmd = Command::cargo_bin("ba2a")?;
     cmd.arg(file.path());
@@ -17,6 +24,26 @@ fn ba2a_test() -> Result<(), Box<std::error::Error>> {
     for kmer in &["ATA", "ATT", "GTT", "TTT"] {
         cmd.assert().stdout(contains(*kmer));
     }
+
+    Ok(())
+}
+
+#[test]
+fn ba2c_test() -> Result<(), Box<std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(
+        file,
+        "ACCTGTTTATTGCCTAAGTTCCGAACAAACCCAATATAGCCCGAGGGCCT
+5
+0.2 0.2 0.3 0.2 0.3
+0.4 0.3 0.1 0.5 0.1
+0.3 0.3 0.5 0.2 0.4
+0.1 0.2 0.1 0.1 0.2"
+    )?;
+
+    let mut cmd = Command::cargo_bin("ba2c")?;
+    cmd.arg(file.path());
+    cmd.assert().success().stdout("CCGAG\n");
 
     Ok(())
 }
